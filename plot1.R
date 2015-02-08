62e48ce9e4d3f94016e1e82c507794382ea4fdc2
Siddhant Mohalanobish <me@siddhant.name>
2015-02-08 14:27:45 -0500

    Plot 1 complete

---
+++ plot1.R
@@ -0,0 +1,18 @@
+# plot1.R
+# Syd-
+# -------------------------------------------------------------
+# Load the data
+allData <- read.csv("household_power_consumption.txt", sep=";", header = TRUE, colClasses = c(rep("character", 9)))
+# Coerce the date so we can subset it
+allData$Date <-  as.Date(strptime(allData$Date, "%d/%m/%Y"))
+# Subset the data on 1st & 2nd of Febuary, 2007
+data <- subset(allData, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+# -------------------------------------------------------------
+# Coerce Global active power as a number
+data$Global_active_power <- as.numeric(data$Global_active_power)
+# Draw the histogram of Global active power vs Frequency
+hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power")
+# Copy the histogram to a png file (with correct dimensions)
+dev.copy(png, file="plot1.png", height=480, width=480)
+# Turn off dev
+dev.off()
